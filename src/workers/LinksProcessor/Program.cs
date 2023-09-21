227e5097f3e57c9fdb854dbdcea68f40b9c64675
Jason LeBel <jason@habaner.io>
2023-09-21 07:22:37 -0400

    Added a Common project and a LinksProcessor Worker Service

---
+++ src/workers/LinksProcessor/Program.cs
@@ -0,0 +1,16 @@
+namespace Deliscio.Workers.LinksProcessor;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        IHost host = Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddHostedService<Worker>();
+            })
+            .Build();
+
+        host.Run();
+    }
+}
