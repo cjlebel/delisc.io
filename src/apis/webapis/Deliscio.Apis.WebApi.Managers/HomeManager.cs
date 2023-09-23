4c0e1f882eb0fae776c44fae44217657d20d8b4f
Jason LeBel <jason@habaner.io>
2023-09-23 17:40:21 -0400

    HomeManager

--- src/apis/webapis/Deliscio.Apis.WebApi.Managers/HomeManager.cs
+++ src/apis/webapis/Deliscio.Apis.WebApi.Managers/HomeManager.cs
@@ -1,4 +1,15 @@
+using Ardalis.GuardClauses;
+using Deliscio.Apis.WebApi.Common.Abstracts;
 using Deliscio.Apis.WebApi.Common.Interfaces;
+using Deliscio.Core.Models;
+using Deliscio.Modules.Links.Common.Models;
+using Deliscio.Modules.Links.MediatR.Queries;
+using Deliscio.Modules.QueuedLinks.Common.Models;
+using MassTransit;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using Structurizr.Annotations;
+
 
 namespace Deliscio.Apis.WebApi.Managers;
 
@@ -7,7 +18,9 @@ namespace Deliscio.Apis.WebApi.Managers;
 /// and returning it to the api in a form that is ready to be consumed by the UI.
 /// </summary>
 /// <seealso cref="IHomeManager" />
-public class HomeManager : IHomeManager
+public class HomeManager : ManagerBase<HomeManager>, IHomeManager
 {
-
+    public HomeManager(IMediator mediator, IBusControl bus, ILogger<HomeManager> logger) : base(bus, logger)
+    {
+    }
 }
